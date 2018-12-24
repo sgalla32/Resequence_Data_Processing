4dd1957cd81b5de76570b1691b99f438b2cecdee
Stephanie Galla <sgalla32@gmail.com>
2018-12-24 14:42:44 +1300

    FastQC_Resequencing.sh
    Used to run FastQC on all raw resequencing files.

---
+++ FastQC_Resequencing.sh
@@ -0,0 +1,92 @@
+#!/bin/sh
+
+/share/data3/conda/miniconda/bin/fastqc /share/data3/sga94/IKMB_Data/fastq/H01383-L1_S41_L007_R1_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01383-L1_S41_L007_R2_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01384-L1_S42_L007_R1_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01384-L1_S42_L007_R2_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01385-L1_S43_L007_R1_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01385-L1_S43_L007_R2_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01386-L1_S1_L001_R1_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01386-L1_S1_L001_R2_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01387-L1_S2_L001_R1_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01387-L1_S2_L001_R2_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01388-L1_S3_L002_R1_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01388-L1_S3_L002_R2_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01389-L1_S4_L003_R1_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01389-L1_S4_L003_R2_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01390-L1_S5_L003_R1_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01390-L1_S5_L003_R2_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01391-L1_S6_L004_R1_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01391-L1_S6_L004_R2_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01392-L1_S7_L005_R1_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01392-L1_S7_L005_R2_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01393-L1_S8_L005_R1_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01393-L1_S8_L005_R2_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01394-L1_S9_L005_R1_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01394-L1_S9_L005_R2_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01395-L1_S10_L007_R1_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01395-L1_S10_L007_R2_001.fq \ 
+/share/data3/sga94/IKMB_Data/fastq/H01396-L1_S11_L007_R1_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01396-L1_S11_L007_R2_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01397-L1_S12_L007_R1_001.fq \ 
+/share/data3/sga94/IKMB_Data/fastq/H01397-L1_S12_L007_R2_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01398-L1_S1_L001_R1_001.fq \ 
+/share/data3/sga94/IKMB_Data/fastq/H01398-L1_S1_L001_R2_001.fq \ 
+/share/data3/sga94/IKMB_Data/fastq/H01399-L1_S2_L001_R1_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01399-L1_S2_L001_R2_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01400-L1_S3_L001_R1_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01400-L1_S3_L001_R2_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01401-L1_S1_L001_R1_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01401-L1_S1_L001_R2_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01402-L1_S2_L001_R1_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01402-L1_S2_L001_R2_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01403-L1_S3_L001_R1_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01403-L1_S3_L001_R2_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01404-L1_S7_L005_R1_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01405-L1_S8_L005_R1_001.fq \ 
+/share/data3/sga94/IKMB_Data/fastq/H01405-L1_S8_L005_R2_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01406-L1_S9_L005_R1_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01406-L1_S9_L005_R2_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01407-L1_S35_L006_R1_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01407-L1_S35_L006_R2_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01408-L1_S36_L006_R1_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01408-L1_S36_L006_R2_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01409-L1_S37_L006_R1_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01409-L1_S37_L006_R2_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01410-L1_S38_L006_R1_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01410-L1_S38_L006_R2_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01411-L1_S39_L006_R1_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01411-L1_S39_L006_R2_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01412-L1_S40_L006_R1_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01412-L1_S40_L006_R2_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01413-L1_S10_L007_R1_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01413-L1_S10_L007_R2_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01414-L1_S11_L007_R1_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01414-L1_S11_L007_R2_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01415-L1_S12_L007_R1_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01415-L1_S12_L007_R2_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01416-L1_S1_L001_R1_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01416-L1_S1_L001_R2_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01417-L1_S2_L001_R1_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01417-L1_S2_L001_R2_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01418-L1_S3_L001_R1_001.fq \
+/share/data3/sga94/IKMB_Data/fastq/H01418-L1_S3_L001_R2_001.fq 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
