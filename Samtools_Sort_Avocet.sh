e6961caa47be09dbff4c329de06ecb655e58e19f
Stephanie Galla <sgalla32@gmail.com>
2018-12-24 15:25:30 +1300

    Create Samtools_Sort_Avocet.sh

---
+++ Samtools_Sort_Avocet.sh
@@ -0,0 +1,75 @@
+#/bin/sh
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage/H01383_Concat_1set_Avocet.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage_sorted/H01383_Concat_1set_Avocet.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage/H01384_Concat_1set_Avocet.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage_sorted/H01384_Concat_1set_Avocet.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage/H01385_Concat_1set_Avocet.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage_sorted/H01385_Concat_1set_Avocet.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage/H01386_Concat_1set_Avocet.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage_sorted/H01386_Concat_1set_Avocet.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage/H01387_Concat_1set_Avocet.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage_sorted/H01387_Concat_1set_Avocet.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage/H01388_Concat_1set_Avocet.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage_sorted/H01388_Concat_1set_Avocet.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage/H01389_Concat_1set_Avocet.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage_sorted/H01389_Concat_1set_Avocet.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage/H01390_Concat_1set_Avocet.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage_sorted/H01390_Concat_1set_Avocet.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage/H01391_Concat_1set_Avocet.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage_sorted/H01391_Concat_1set_Avocet.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage/H01392_Concat_1set_Avocet.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage_sorted/H01392_Concat_1set_Avocet.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage/H01393_Concat_1set_Avocet.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage_sorted/H01393_Concat_1set_Avocet.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage/H01394_Concat_1set_Avocet.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage_sorted/H01394_Concat_1set_Avocet.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage/H01395_Concat_1set_Avocet.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage_sorted/H01395_Concat_1set_Avocet.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage/H01396_Concat_1set_Avocet.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage_sorted/H01396_Concat_1set_Avocet.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage/H01397_Concat_1set_Avocet.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage_sorted/H01397_Concat_1set_Avocet.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage/H01397_Concat_1set_Avocet.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage_sorted/H01397_Concat_1set_Avocet.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage/H01398_Concat_1set_Avocet.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage_sorted/H01398_Concat_1set_Avocet.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage/H01399_Concat_1set_Avocet.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage_sorted/H01399_Concat_1set_Avocet.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage/H01400_Concat_1set_Avocet.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage_sorted/H01400_Concat_1set_Avocet.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage/H01401_Concat_1set_Avocet.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage_sorted/H01401_Concat_1set_Avocet.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage/H01402_Concat_1set_Avocet.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage_sorted/H01402_Concat_1set_Avocet.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage/H01403_Concat_1set_Avocet.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage_sorted/H01403_Concat_1set_Avocet.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage/H01404_Concat_1set_Avocet.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage_sorted/H01404_Concat_1set_Avocet.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage/H01405_Concat_1set_Avocet.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage_sorted/H01405_Concat_1set_Avocet.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage/H01406_Concat_1set_Avocet.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage_sorted/H01406_Concat_1set_Avocet.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage/H01407_Avocet.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage_sorted/H01407_Avocet.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage/H01408_Avocet.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage_sorted/H01408_Avocet.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage/H01409_Avocet.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage_sorted/H01409_Avocet.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage/H01410_Avocet.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage_sorted/H01410_Avocet.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage/H01411_Avocet.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage_sorted/H01411_Avocet.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage/H01412_Avocet.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage_sorted/H01412_Avocet.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage/H01413_Avocet.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage_sorted/H01413_Avocet.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage/H01414_Avocet.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage_sorted/H01414_Avocet.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage/H01415_Avocet.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage_sorted/H01415_Avocet.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage/H01416_Avocet.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage_sorted/H01416_Avocet.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage/H01417_Avocet.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage_sorted/H01417_Avocet.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage/H01418_Avocet.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Avocet/1set_and_lowcoverage_sorted/H01418_Avocet.sorted' 
