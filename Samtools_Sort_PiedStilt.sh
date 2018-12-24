a1432469652a8e5aa64aa55442618b447a4bc7cf
Stephanie Galla <sgalla32@gmail.com>
2018-12-24 15:26:04 +1300

    Samtools_Sort_PiedStilt.sh

---
+++ Samtools_Sort_PiedStilt.sh
@@ -0,0 +1,75 @@
+#/bin/sh
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage/H01383_Concat_1set_AusPied.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage_sorted/H01383_Concat_1set_AusPied.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage/H01384_Concat_1set_AusPied.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage_sorted/H01384_Concat_1set_AusPied.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage/H01385_Concat_1set_AusPied.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage_sorted/H01385_Concat_1set_AusPied.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage/H01386_Concat_1set_AusPied.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage_sorted/H01386_Concat_1set_AusPied.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage/H01387_Concat_1set_AusPied.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage_sorted/H01387_Concat_1set_AusPied.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage/H01388_Concat_1set_AusPied.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage_sorted/H01388_Concat_1set_AusPied.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage/H01389_Concat_1set_AusPied.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage_sorted/H01389_Concat_1set_AusPied.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage/H01390_Concat_1set_AusPied.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage_sorted/H01390_Concat_1set_AusPied.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage/H01391_Concat_1set_AusPied.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage_sorted/H01391_Concat_1set_AusPied.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage/H01392_Concat_1set_AusPied.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage_sorted/H01392_Concat_1set_AusPied.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage/H01393_Concat_1set_AusPied.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage_sorted/H01393_Concat_1set_AusPied.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage/H01394_Concat_1set_AusPied.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage_sorted/H01394_Concat_1set_AusPied.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage/H01395_Concat_1set_AusPied.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage_sorted/H01395_Concat_1set_AusPied.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage/H01396_Concat_1set_AusPied.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage_sorted/H01396_Concat_1set_AusPied.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage/H01397_Concat_1set_AusPied.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage_sorted/H01397_Concat_1set_AusPied.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage/H01397_Concat_1set_AusPied.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage_sorted/H01397_Concat_1set_AusPied.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage/H01398_Concat_1set_AusPied.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage_sorted/H01398_Concat_1set_AusPied.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage/H01399_Concat_1set_AusPied.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage_sorted/H01399_Concat_1set_AusPied.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage/H01400_Concat_1set_AusPied.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage_sorted/H01400_Concat_1set_AusPied.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage/H01401_Concat_1set_AusPied.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage_sorted/H01401_Concat_1set_AusPied.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage/H01402_Concat_1set_AusPied.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage_sorted/H01402_Concat_1set_AusPied.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage/H01403_Concat_1set_AusPied.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage_sorted/H01403_Concat_1set_AusPied.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage/H01404_Concat_1set_AusPied.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage_sorted/H01404_Concat_1set_AusPied.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage/H01405_Concat_1set_AusPied.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage_sorted/H01405_Concat_1set_AusPied.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage/H01406_Concat_1set_AusPied.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage_sorted/H01406_Concat_1set_AusPied.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage/H01407_AusPied.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage_sorted/H01407_AusPied.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage/H01408_AusPied.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage_sorted/H01408_AusPied.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage/H01409_AusPied.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage_sorted/H01409_AusPied.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage/H01410_AusPied.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage_sorted/H01410_AusPied.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage/H01411_AusPied.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage_sorted/H01411_AusPied.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage/H01412_AusPied.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage_sorted/H01412_AusPied.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage/H01413_AusPied.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage_sorted/H01413_AusPied.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage/H01414_AusPied.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage_sorted/H01414_AusPied.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage/H01415_AusPied.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage_sorted/H01415_AusPied.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage/H01416_AusPied.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage_sorted/H01416_AusPied.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage/H01417_AusPied.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage_sorted/H01417_AusPied.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage/H01418_AusPied.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/AusPied/1set_and_lowcoverage_sorted/H01418_AusPied.sorted' 
