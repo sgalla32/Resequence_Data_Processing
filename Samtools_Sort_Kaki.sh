5ec20f6b0bc3327bff53ceacad951275b0a77b69
Stephanie Galla <sgalla32@gmail.com>
2018-12-24 15:22:18 +1300

    Samtools_Sort_Kaki.sh

---
+++ Samtools_Sort_Kaki.sh
@@ -0,0 +1,75 @@
+#/bin/sh
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage/H01383_Concat_1set_kaki.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage_sorted/H01383_Concat_1set_Kaki.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage/H01384_Concat_1set_kaki.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage_sorted/H01384_Concat_1set_Kaki.sorted' 
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage/H01385_Concat_1set_kaki.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage_sorted/H01385_Concat_1set_Kaki.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage/H01386_Concat_1set_kaki.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage_sorted/H01386_Concat_1set_Kaki.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage/H01387_Concat_1set_kaki.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage_sorted/H01387_Concat_1set_Kaki.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage/H01388_Concat_1set_kaki.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage_sorted/H01388_Concat_1set_Kaki.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage/H01389_Concat_1set_kaki.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage_sorted/H01389_Concat_1set_Kaki.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage/H01390_Concat_1set_kaki.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage_sorted/H01390_Concat_1set_Kaki.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage/H01391_Concat_1set_kaki.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage_sorted/H01391_Concat_1set_Kaki.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage/H01392_Concat_1set_kaki.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage_sorted/H01392_Concat_1set_Kaki.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage/H01393_Concat_1set_kaki.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage_sorted/H01393_Concat_1set_Kaki.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage/H01394_Concat_1set_kaki.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage_sorted/H01394_Concat_1set_Kaki.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage/H01395_Concat_1set_kaki.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage_sorted/H01395_Concat_1set_Kaki.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage/H01396_Concat_1set_kaki.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage_sorted/H01396_Concat_1set_Kaki.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage/H01397_Concat_1set_kaki.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage_sorted/H01397_Concat_1set_Kaki.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage/H01397_Concat_1set_kaki.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage_sorted/H01397_Concat_1set_Kaki.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage/H01398_Concat_1set_kaki.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage_sorted/H01398_Concat_1set_Kaki.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage/H01399_Concat_1set_kaki.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage_sorted/H01399_Concat_1set_Kaki.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage/H01400_Concat_1set_kaki.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage_sorted/H01400_Concat_1set_Kaki.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage/H01401_Concat_1set_kaki.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage_sorted/H01401_Concat_1set_Kaki.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage/H01402_Concat_1set_kaki.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage_sorted/H01402_Concat_1set_Kaki.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage/H01403_Concat_1set_kaki.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage_sorted/H01403_Concat_1set_Kaki.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage/H01404_Concat_1set_kaki.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage_sorted/H01404_Concat_1set_Kaki.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage/H01405_Concat_1set_kaki.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage_sorted/H01405_Concat_1set_Kaki.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage/H01406_Concat_1set_kaki.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage_sorted/H01406_Concat_1set_Kaki.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage/H01407_Kaki.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage_sorted/H01407_Kaki.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage/H01408_Kaki.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage_sorted/H01408_Kaki.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage/H01409_Kaki.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage_sorted/H01409_Kaki.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage/H01410_Kaki.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage_sorted/H01410_Kaki.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage/H01411_Kaki.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage_sorted/H01411_Kaki.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage/H01412_Kaki.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage_sorted/H01412_Kaki.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage/H01413_Kaki.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage_sorted/H01413_Kaki.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage/H01414_Kaki.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage_sorted/H01414_Kaki.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage/H01415_Kaki.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage_sorted/H01415_Kaki.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage/H01416_Kaki.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage_sorted/H01416_Kaki.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage/H01417_Kaki.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage_sorted/H01417_Kaki.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage/H01418_Kaki.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Kaki/1set_and_lowcoverage_sorted/H01418_Kaki.sorted'
