7a4526eb07fd79a0d679d49ed55bd726dce5d161
Stephanie Galla <sgalla32@gmail.com>
2018-12-24 15:24:41 +1300

    Samtools_Sort_Killdeer.sh

---
+++ Samtools_Sort_Killdeer.sh
@@ -0,0 +1,53 @@
+#/bin/sh
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Bam_Files/H01393_Concat_1set_Killdeer.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Sorted_Bam_Files/H01393_Concat_1set_Killdeer.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Bam_Files/H01395_Concat_1set_Killdeer.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Sorted_Bam_Files/H01395_Concat_1set_Killdeer.sorted.bam' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Bam_Files/H01396_Concat_1set_Killdeer.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Sorted_Bam_Files/H01396_Concat_1set_Killdeer.sorted.bam' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Bam_Files/H01397_Concat_1set_Killdeer.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Sorted_Bam_Files/H01397_Concat_1set_Killdeer.sorted.bam' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Bam_Files/H01397_Concat_1set_Killdeer.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Sorted_Bam_Files/H01397_Concat_1set_Killdeer.sorted.bam' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Bam_Files/H01398_Concat_1set_Killdeer.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Sorted_Bam_Files/H01398_Concat_1set_Killdeer.sorted.bam' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Bam_Files/H01399_Concat_1set_Killdeer.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Sorted_Bam_Files/H01399_Concat_1set_Killdeer.sorted.bam' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Bam_Files/H01400_Concat_1set_Killdeer.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Sorted_Bam_Files/H01400_Concat_1set_Killdeer.sorted.bam' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Bam_Files/H01401_Concat_1set_Killdeer.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Sorted_Bam_Files/H01401_Concat_1set_Killdeer.sorted.bam' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Bam_Files/H01402_Concat_1set_Killdeer.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Sorted_Bam_Files/H01402_Concat_1set_Killdeer.sorted.bam' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Bam_Files/H01403_Concat_1set_Killdeer.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Sorted_Bam_Files/H01403_Concat_1set_Killdeer.sorted.bam' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Bam_Files/H01404_Concat_1set_Killdeer.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Sorted_Bam_Files/H01404_Concat_1set_Killdeer.sorted.bam' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Bam_Files/H01405_Concat_1set_Killdeer.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Sorted_Bam_Files/H01405_Concat_1set_Killdeer.sorted.bam' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Bam_Files/H01406_Concat_1set_Killdeer.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Sorted_Bam_Files/H01406_Concat_1set_Killdeer.sorted.bam' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Bam_Files/H01407_Killdeer.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Sorted_Bam_Files/H01407_Killdeer.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Bam_Files/H01408_Killdeer.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Sorted_Bam_Files/H01408_Killdeer.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Bam_Files/H01409_Killdeer.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Sorted_Bam_Files/H01409_Killdeer.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Bam_Files/H01410_Killdeer.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Sorted_Bam_Files/H01410_Killdeer.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Bam_Files/H01411_Killdeer.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Sorted_Bam_Files/H01411_Killdeer.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Bam_Files/H01412_Killdeer.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Sorted_Bam_Files/H01412_Killdeer.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Bam_Files/H01413_Killdeer.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Sorted_Bam_Files/H01413_Killdeer.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Bam_Files/H01414_Killdeer.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Sorted_Bam_Files/H01414_Killdeer.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Bam_Files/H01415_Killdeer.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Sorted_Bam_Files/H01415_Killdeer.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Bam_Files/H01416_Killdeer.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Sorted_Bam_Files/H01416_Killdeer.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Bam_Files/H01417_Killdeer.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Sorted_Bam_Files/H01417_Killdeer.sorted' \
+
+samtools sort '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Bam_Files/H01418_Killdeer.bam' '/media/stephanie/Data/Resequencing/Bowtie_BAM_Files/Killdeer/Sorted_Bam_Files/H01418_Killdeer.sorted' \
